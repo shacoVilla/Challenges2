c81cba67a425765688b028fd7642376202fb1d6d
daniel bello <beda@erni-espana.es>
2019-08-21 00:51:00 +0200

    Student model override tostring

--- AnonymousPoll/AnonymousPoll/Model/Student.cs
+++ AnonymousPoll/AnonymousPoll/Model/Student.cs
@@ -2,27 +2,39 @@
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
-namespace AnonymousPoll.Core.Model
+namespace AnonymousPoll.Model
 {
+    using AnonymousPoll.Core.Input;
+
     public class Student
     {
-        public Student(string name, string gender, string age, string studies, string academicYear)
-        {
-            this.Name = name;
-            this.Gender = gender;
-            this.Age = age;
-            this.Studies = studies;
-            this.AcademicYear = academicYear;
-        }
-
         public string Gender { get; set; }
 
         public string Age { get; set; }
 
         public string Name { get; set; }
 
-        public string Studies { get; set; }
+        public string Study { get; set; }
 
         public string AcademicYear { get; set; }
+
+        public Student(string name, string gender, string age, string study, string academicYear)
+        {
+            this.Name = name;
+            this.Gender = gender;
+            this.Age = age;
+            this.Study = study;
+            this.AcademicYear = academicYear;
+        }
+
+        public bool BelongsToStudentCase(InputCase other)
+        {
+            if (this.Gender != other.Gender || this.Age != other.Age || this.Study != other.Study || this.AcademicYear != other.AcademicYear)
+            {
+                return false;
+            }
+
+            return true;
+        }
     }
 }
